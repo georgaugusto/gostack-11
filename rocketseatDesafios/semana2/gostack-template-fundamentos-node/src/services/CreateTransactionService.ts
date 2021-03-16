e306f86f6c7bff2db58e139222a2285e6cd79b08
Georg Augusto Schlegel <georgaugusto@gmail.com>
2021-03-15 23:41:08 -0300

    first commit

---
+++ rocketseatDesafios/semana2/gostack-template-fundamentos-node/src/services/CreateTransactionService.ts
@@ -0,0 +1,38 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    if ((type === 'income' || type === 'outcome') && value <= 0) {
+      throw Error('You must deposit a value greater than zero');
+    }
+
+    if (type === 'outcome' && (balance.total < value || value === 0)) {
+      throw Error('You don`t have enough balance');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
---
+++ rocketseatDesafios/semana2/gostack-template-typeorm-upload/src/repositories/TransactionsRepository.ts
@@ -0,0 +1,41 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+@EntityRepository(Transaction)
+class TransactionsRepository extends Repository<Transaction> {
+  public async getBalance(): Promise<Balance> {
+    const transactions = await this.find();
+
+    const balance: Balance = transactions.reduce(
+      (accumulator, transaction) => {
+        if (transaction.type === 'income') {
+          accumulator.income += Number(transaction.value);
+        }
+
+        if (transaction.type === 'outcome') {
+          accumulator.outcome += Number(transaction.value);
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
+  }
+}
+
+export default TransactionsRepository;
